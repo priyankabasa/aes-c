1bd8ef8e8555ece56efe4823be62e6225138351d
priyanka <basapriyanka02@gmail.com>
2025-04-17 23:28:26 +0100

    Implemented invert_shift_rows  for AES encryption

--- rijndael.c
+++ rijndael.c
@@ -155,8 +155,31 @@ void invert_sub_bytes(unsigned char *block) {
 }
 
 void invert_shift_rows(unsigned char *block) {
-  // TODO: Implement me!
-}
+    unsigned char temp;
+  
+    // Row 1 (right shift by 1)
+    temp = block[13];
+    block[13] = block[9];
+    block[9] = block[5];
+    block[5] = block[1];
+    block[1] = temp;
+  
+    // Row 2 (right shift by 2)
+    temp = block[2];
+    block[2] = block[10];
+    block[10] = temp;
+    temp = block[6];
+    block[6] = block[14];
+    block[14] = temp;
+  
+    // Row 3 (right shift by 3, same as left shift by 1)
+    temp = block[3];
+    block[3] = block[7];
+    block[7] = block[11];
+    block[11] = block[15];
+    block[15] = temp;
+  }
+
 
 void invert_mix_columns(unsigned char *block) {
   // TODO: Implement me!
